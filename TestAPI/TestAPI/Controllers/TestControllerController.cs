f57a4fd95bd16fd0221856238efd2c71fc936346
Lakhwinder <Lakhwinder.chdit@gmail.com>
2025-06-19 11:03:49 +0530

    add test method

---
+++ TestAPI/TestAPI/Controllers/TestControllerController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : ControllerBase
+    {
+        // Sample model
+        public class SampleItem
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+        }
+
+        // Action method with a specific name and route
+        [HttpGet("everything")]
+        public async Task<IActionResult> GetSampleItems()
+        {
+            await Task.Delay(500); // Simulated async delay
+
+            var data = new List<SampleItem>
+            {
+                new SampleItem { Id = 1, Name = "Item One" },
+                new SampleItem { Id = 2, Name = "Item Two" },
+                new SampleItem { Id = 3, Name = "Item Three" }
+            };
+
+            return Ok(data);
+        }
+    }
+}
