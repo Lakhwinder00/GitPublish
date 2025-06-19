fce468c2532c48ff96b3991ad2bf9b1b77b87190
Lakhwinder <Lakhwinder.chdit@gmail.com>
2025-06-19 11:05:55 +0530

    some change method

--- TestAPI/TestAPI/Controllers/TestControllerController.cs
+++ TestAPI/TestAPI/Controllers/TestControllerController.cs
@@ -23,7 +23,8 @@ namespace TestAPI.Controllers
             {
                 new SampleItem { Id = 1, Name = "Item One" },
                 new SampleItem { Id = 2, Name = "Item Two" },
-                new SampleItem { Id = 3, Name = "Item Three" }
+                new SampleItem { Id = 3, Name = "Item Three" },
+                  new SampleItem { Id = 3, Name = "Item Three" }
             };
 
             return Ok(data);
